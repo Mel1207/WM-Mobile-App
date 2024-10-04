ccb3d2107cfc6652a496a116c125e7c46527ad2d
Mel <57514727+Mel1207@users.noreply.github.com>
2024-10-05 05:31:29 +0800

    added landing banner

---
+++ src/components/TagComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TagComp = ({ tagTitle }) => {
+  return (
+    <div className='py-1 text-[12px] px-3 bg-[#651CFF10] text-cViolet rounded-full w-max mb-[10px]'>{tagTitle}</div>
+  )
+}
+
+export default TagComp
