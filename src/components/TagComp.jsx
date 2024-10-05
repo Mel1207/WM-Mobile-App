1007cf25d8078c3cc86b0cfec97645461b2e844d
Mel <57514727+Mel1207@users.noreply.github.com>
2024-10-05 12:36:56 +0800

    added dashboard routing and fix desktop ui styles

--- src/components/TagComp.jsx
+++ src/components/TagComp.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const TagComp = ({ tagTitle }) => {
+const TagComp = ({ tagTitle, classList }) => {
   return (
-    <div className='py-1 text-[12px] px-3 bg-[#651CFF10] text-cViolet rounded-full w-max mb-[10px]'>{tagTitle}</div>
+    <div className={`py-1 text-[12px] px-3 bg-[#651CFF10] text-cViolet rounded-full w-max ${classList}`}>{tagTitle}</div>
   )
 }
 
--- src/pages/PageLanding.jsx
+++ src/pages/PageLanding.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 import Button from '../components/Button'
 import TagComp from '../components/TagComp'
 import mobileLandingImage from '../assets/mobile-landing-image.png'
+import dektopLandingImage from '../assets/desktop-landing-image.png'
+import { Link } from 'react-router-dom'
 
 const PageLanding = () => {
   return (
-    <div className='h-screen flex flex-col font-inter'>
-      {/* <div className="bg-[url('./assets/mobile-landing-bg.png')] h-[55%] bg-center w-full bg-no-repeat bg-cover"></div> */}
-      <div className='py-[40px] px-[20px]'>
-        <img src={mobileLandingImage} alt="Mobile landing image" className='h-[422px] object-cover rounded-[20px] w-full'/>
+    <div className='h-screen flex flex-col font-inter md:flex-row'>
+      <div className='py-[40px] px-[20px] md:p-0 md:w-[60%]'>
+        <img src={mobileLandingImage} alt="Mobile landing image" className='h-[422px] object-cover rounded-[20px] md:h-full w-full md:hidden'/>
+        <img className='hidden md:block h-full object-cover' src={dektopLandingImage} aria-label='desktop banner' alt='desktop label' />
       </div>
-      <div className='p-[20px] flex flex-col align-middle h-auto justify-center'>
-        <TagComp tagTitle='Featured app'/>
-        <h1 className=' text-[20px] font-semibold mb-[10px]'>Manage your cards fast and easy <br />with Walletmate</h1>
-        <p className='text-cBlack10 mb-[20px]'>Join over a million+ users and enjoy fast and secure digital banking solutions.</p>
-        <Button classList='rounded-md mb-4 h-[45px] bg-cViolet btn-primary w-full text-white' btnTitle='Get started'/>
-        <Button btnTitle='Create an account' classList='btn rounded-md w-full h-[45px] border border-gray-900'/>
+      <div className='px-5 max-w-[440px] flex flex-col align-middle mx-auto md:w-[548px] justify-center'>
+        <TagComp tagTitle='Featured app' classList='mb-[10px] md:mb-[15px]'/>
+        <h1 className='text-[20px] md:text-[25px] font-semibold mb-[10px] text-cBlack10'>Manage your cards fast and easy <br />with Walletmate</h1>
+        <p className='text-cBlack20 mb-[20px]'>Join over a million+ users and enjoy fast and secure digital banking solutions.</p>
+        <div className='flex gap-[15px] flex-col md:flex-row md:gap-[15px]'>
+          <Link to='/dashboard' className='block w-full'>
+            <Button classList='btn-primary' btnTitle='Get started'/>
+          </Link>
+          <Button classList='btn-outlined-secondary' btnTitle='Create an account' />
+        </div>
       </div>
     </div>
   )
