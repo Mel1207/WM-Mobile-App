2fae71b5dd5faa9573599165ccfa24b1063466b2
Mel <57514727+Mel1207@users.noreply.github.com>
2024-10-05 14:05:59 +0800

    added mobile navigation component

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,30 @@
-export const navLinks = [
+import iconDashboard1 from '../assets/icon-dashboard.svg'
+import iconDashboard2 from '../assets/icon-dashboard-active.svg'
+import iconCard1 from '../assets/icon-card.svg'
+import iconCard2 from '../assets/icon-card-active.svg'
+import transactions1 from '../assets/icon-transaction.svg'
+import transactions2 from '../assets/icon-transaction-active.svg'
+
+export const mobileLinks = [
   {
     id: 1, 
-    path: '/',
-    title: 'Landing',
+    path: '/dashboard',
+    title: 'Dashboard',
+    iconActive: iconDashboard2,
+    icon: iconDashboard1
   },
   {
     id: 2, 
-    path: '/dashboard',
-    title: 'Dashboard',
+    path: '/cards',
+    title: 'My cards',
+    iconActive: iconCard2,
+    icon: iconCard1
   },
   {
     id: 3, 
-    path: '/cards',
-    title: 'My cards',
-  },
-  {
-    id: 4, 
     path: '/transactions',
     title: 'Transactions',
-  },
+    iconActive: transactions2,
+    icon: transactions1
+  }
 ]
